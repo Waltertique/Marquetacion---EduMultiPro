a8572da2baf8a0cec14feb00167a040113423bbb
Waltertique <johanmadrigal66@gmail.com>
2025-01-29 12:47:43 -0500

    eliminacion de archivos

---
+++ Estudiante/js/14.js
@@ -0,0 +1,4 @@
+document.getElementById('salir').addEventListener('click', function () {
+    // Redirige a otra página
+    window.location.href = '../Profesor/13-calificaciones.html';
+});
---
+++ Estudiante/js/16.js
@@ -0,0 +1,10 @@
+
+document.getElementById('descargar').addEventListener('click', function () {
+  // Redirige a otra página
+  window.location.href = '../Estudiante/4-horario.html';
+});
+
+document.getElementById('salir').addEventListener('click', function () {
+  // Redirige a otra página
+  window.location.href = '../Estudiante/13-calificaciones.html';
+});
