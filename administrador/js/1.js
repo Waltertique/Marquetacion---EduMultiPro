82504fd34b548aa218dbeb4d9aa5a5b844fe5293
Waltertique <johanmadrigal66@gmail.com>
2025-01-29 20:55:46 -0500

    tabla de usuarios

---
+++ administrador/js/1.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#tablaUsuarios').DataTable({
+       language:{
+        processing:"Procesando...",
+        search:"Buscar:",
+        lengthMenu:"Mostrar  _MENU_  registros",
+        info:"Mostrando _START_ a _END_ de _TOTAL_ registros",
+        infoEmpty: "Mostrando 0 a 0 de 0 registros",
+        infoFiltered:"(filtrado de _MAX_ registros totales)",
+        loadingRecords: "Cargando...",
+        zeroRecords: "No se encontraron resultados",
+        emptyTable: "No hay datos en la tabla",
+        paginate:{
+            previous: "Anterior", 
+            next:"Siguiente",
+        },
+        aria:{
+            sortAscending:"Activar para ordenar la columna ascendentemente",
+            sortDescending:"Activar para ordenar la columna descendentemente",
+        }
+       },
+       paging:true,
+       searching:true,
+       ordering:true 
+    });
+    });
